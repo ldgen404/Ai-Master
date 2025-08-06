355626a0b48c94adb03a6772b6e976ff4eeb6ca4
xun.li <1538574120@qq.com>
2025-08-06 12:44:26 +0800

    AI-应用生成-基础CURD完成

---
+++ src/main/java/com/ldgen/ldgenaimaster/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.ldgen.ldgenaimaster.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.ldgen.ldgenaimaster.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author <a href="https://github.com/ldgen404/">程序员李大根</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
--- src/main/java/com/ldgen/ldgenaimaster/mapper/UserMapper.java
+++ src/main/java/com/ldgen/ldgenaimaster/mapper/UserMapper.java
@@ -6,7 +6,7 @@ import com.ldgen.ldgenaimaster.model.entity.User;
 /**
  * 用户 映射层。
  *
- * @author <a href="https://github.com/ldgen404/">程序员李大根</a>
+ * @author <a href="https://github.com/ldgen404/">程序员李大根 </a>
  */
 public interface UserMapper extends BaseMapper<User> {
 
